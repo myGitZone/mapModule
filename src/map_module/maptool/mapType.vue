73a5b137bc415fc65cfd80b36b3e3557ab81b92d
yanchanglu <513894105@qq.com>
2017-07-17 15:27:58 +0800

    修改大图标为小图标。并修改版本号

--- src/map_module/maptool/mapType.vue
+++ src/map_module/maptool/mapType.vue
@@ -15,96 +15,95 @@
 </template>
 
 <script>
-export default {
-  name: 'MapType',
-  data() {
-    return {
-      firstType: {
-        class: 'normal',
-        name: '矢量'
-      },
-      secondType: {
-        class: 'earch',
-        name: '地形'
-      },
-      thirdType: {
-        class: 'terrain',
-        name: '影像'
-      },
-      layers: {
-        '矢量': {
-          baseMapName: '矢量底图',
-          baseLabelName: '矢量标注'
+  export default {
+    name: 'MapType',
+    data() {
+      return {
+        firstType: {
+          class: 'normal',
+          name: '矢量'
         },
-        '地形': {
-          baseMapName: '地形底图',
-          baseLabelName: '地形标注'
+        secondType: {
+          class: 'earch',
+          name: '地形'
         },
-        '影像': {
-          baseMapName: '影像底图',
-          baseLabelName: '影像标注'
+        thirdType: {
+          class: 'terrain',
+          name: '影像'
+        },
+        layers: {
+          '矢量': {
+            baseMapName: '矢量底图',
+            baseLabelName: '矢量标注'
+          },
+          '地形': {
+            baseMapName: '地形底图',
+            baseLabelName: '地形标注'
+          },
+          '影像': {
+            baseMapName: '影像底图',
+            baseLabelName: '影像标注'
+          }
+        }
+      };
+    },
+    props: {
+      // 基础配置
+      mapConfig: {
+        map: {
+          type: Object,
+          required: true
+        },
+        esriLoader: {
+          type: Object,
+          required: true
         }
       }
-    };
-  },
-  props: {
-    // 基础配置
-    mapConfig: {
-      map: {
-        type: Object,
-        required: true
-      },
-      esriLoader: {
-        type: Object,
-        required: true
+    },
+    methods: {
+      changeMapType: function (index) {
+        if (!this.mapConfig.map.getLayer('baseMap')) return;
+        let _self = this;
+        // 记录当前选中的类型
+        let currentTypeName = null;
+        let tempObj = null;
+        switch (index) {
+          case 0:
+            return;
+          case 1:
+            currentTypeName = this.secondType.name;
+            tempObj = this.firstType;
+            this.firstType = this.secondType;
+            this.secondType = tempObj;
+            break;
+          case 2:
+            currentTypeName = this.thirdType.name;
+            tempObj = this.firstType;
+            this.firstType = this.thirdType;
+            this.thirdType = tempObj;
+            break;
+          default:
+            return;
+        }
+        let {baseMapName, baseLabelName} = this.layers[currentTypeName];
+        this.mapConfig.esriLoader.dojoRequire(['esri/map', 'esri/geometry/Extent', 'tdlib/TianDiTuLayer'], (Map, Extent, TianDiTuLayer) => {
+          let baseMapLayer = new TianDiTuLayer(baseMapName, {
+            id: 'baseMap'
+          });
+          let baseLabelLayer = new TianDiTuLayer(baseLabelName, {
+            id: 'baseLabel'
+          });
+          _self.mapConfig.map.removeLayer(_self.mapConfig.map.getLayer('baseMap'));
+          _self.mapConfig.map.removeLayer(_self.mapConfig.map.getLayer('baseLabel'));
+          _self.mapConfig.map.addLayer(baseMapLayer, 0);
+          _self.mapConfig.map.addLayer(baseLabelLayer, 1);
+          _self.active = event.target.id;
+          // 触发鹰眼的mapTypeChange -by overviewMap.vue
+          _self.$events.$emit('mapTypeChange', {baseMap: baseMapLayer, baseLabel: baseLabelLayer});
+        });
       }
     }
-  },
-  methods: {
-    changeMapType: function (index) {
-      if (!this.mapConfig.map.getLayer('baseMap')) return;
-      let _self = this;
-      // 记录当前选中的类型
-      let currentTypeName = null;
-      let tempObj = null;
-      switch (index) {
-        case 0:
-          return;
-        case 1:
-          currentTypeName = this.secondType.name;
-          tempObj = this.firstType;
-          this.firstType = this.secondType;
-          this.secondType = tempObj;
-          break;
-        case 2:
-          currentTypeName = this.thirdType.name;
-          tempObj = this.firstType;
-          this.firstType = this.thirdType;
-          this.thirdType = tempObj;
-          break;
-        default:
-          return;
-      }
-      let { baseMapName, baseLabelName } = this.layers[currentTypeName];
-      debugger;
-      this.mapConfig.esriLoader.dojoRequire(['esri/map', 'esri/geometry/Extent', 'tdlib/TianDiTuLayer'], (Map, Extent, TianDiTuLayer) => {
-        let baseMapLayer = new TianDiTuLayer(baseMapName, {
-          id: 'baseMap'
-        });
-        let baseLabelLayer = new TianDiTuLayer(baseLabelName, {
-          id: 'baseLabel'
-        });
-        _self.mapConfig.map.removeLayer(_self.mapConfig.map.getLayer('baseMap'));
-        _self.mapConfig.map.removeLayer(_self.mapConfig.map.getLayer('baseLabel'));
-        _self.mapConfig.map.addLayer(baseMapLayer, 0);
-        _self.mapConfig.map.addLayer(baseLabelLayer, 1);
-        _self.active = event.target.id;
-        // 触发鹰眼的mapTypeChange -by overviewMap.vue
-        _self.$events.$emit('mapTypeChange', { baseMap: baseMapLayer, baseLabel: baseLabelLayer });
-      });
-    }
-  }
-};
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
   .maptype-container
@@ -117,7 +116,7 @@ export default {
         .mapTypeCard
           width: 80px
           margin-right: 10px
-          background-size:100% 100%
+          background-size: 100% 100%
           span
             display: inline
       .mapTypeCard
@@ -125,21 +124,21 @@ export default {
         height: 60px
         width: 10px
         float: left
-        background-size:100% 100% !important
+        background-size: 100% 100% !important
         cursor: pointer
         -webkit-transition: width 0.2s ease-out
         -moz-transition: width 0.2s ease-out
         -ms-transition: width 0.2s ease-out
         transition: width 0.2s ease-out
         list-style-type: none
-        text-align:right;
+        text-align: right;
         span
-          background-color:#3385FF
+          background-color: #3385FF
           display: none
       .active
         width: 80px
         span
-            display: inline
+          display: inline
       .normal
         background: #999 url(../image/map/maptype/shiliang.png) no-repeat
       .earch
